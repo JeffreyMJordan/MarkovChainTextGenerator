eaa412b2f950029566bf6a01649dbe16f331068c
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-04 12:32:41 -0800

    JS presets and node packages

---
+++ root.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("here");
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>It's working</h1>, root);
+});
